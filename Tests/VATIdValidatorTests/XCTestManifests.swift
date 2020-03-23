2c9e95e34c5db42815bf0cc7e770c0f3f0e9bc3a
Paweł Bednorz <pawel@quver.pl>
2020-03-23 01:46:00 +0100

    Folder name fix 2

--- Tests/VATIdValidatorTests/XCTestManifests.swift
+++ Tests/VATIdValidatorTests/XCTestManifests.swift
@@ -2,8 +2,6 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(VATIDValidatorTests.allTests)  
-    ]
+    return [testCase(VATIdValidatorTests.allTests)]
 }
 #endif
