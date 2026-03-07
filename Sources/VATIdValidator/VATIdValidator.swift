15ffe8f8c76e118bd77e74222fbe4cb55b8b4556
Paweł Bednorz <pawel@quver.pl>
2026-03-07 14:13:15 +0100

    chore: update swiftlint configuration

--- Sources/VATIdValidator/VATIdValidator.swift
+++ Sources/VATIdValidator/VATIdValidator.swift
@@ -1,5 +1,5 @@
 public struct VATIdValidator {
-    
+
     /**
      ## Possible validation errors.
      
@@ -10,16 +10,16 @@ public struct VATIdValidator {
 
         /// The VAT Identifier should have 10 digits.
         case incorrectLength
-        
+
         /// Checksum should be equal 10th digit of the VAT ID.
         case checkSumNotMatch
-        
+
     }
-    
+
     let vatId: [Int?]
     private let filteredVATId: [Int]
     private let vatIdLength = 10
-    
+
     /**
      Common constructor.
      - Parameter vatId: The VAT identifier as array of integers.
@@ -28,7 +28,7 @@ public struct VATIdValidator {
         self.vatId = vatId
         self.filteredVATId = vatId.compactMap { $0 }
     }
-    
+
     /**
      Common constructor.
      - Parameter vatId: The VAT identifier as any integer.
@@ -36,7 +36,7 @@ public struct VATIdValidator {
     public init<T: BinaryInteger>(_ vatId: T) {
         self.init(String(vatId))
     }
-    
+
     /**
      Common constructor. Converts double to integer.
      - Parameter vatId: The VAT identifier as double.
@@ -44,7 +44,7 @@ public struct VATIdValidator {
     public init(_ vatId: Double) {
         self.init(Int(vatId))
     }
-    
+
     /**
      Common constructor.
      - Parameter vatId: The VAT identifier as string.
@@ -64,14 +64,14 @@ public struct VATIdValidator {
         // The VAT identifier should have 10 digits.
         guard vatId.count == vatIdLength
             && filteredVATId.count == vatIdLength else { throw ValidationError.incorrectLength }
-        
+
         // Checksum should be equal 10th digit of the VAT identifier.
         guard checkSum() == vatId[9] else { throw ValidationError.checkSumNotMatch }
     }
-    
+
     func checkSum() -> Int? {
         guard vatId.count == vatIdLength && filteredVATId.count == vatIdLength else { return nil }
-        
+
         return (6 * filteredVATId[0] +
             5 * filteredVATId[1] +
             7 * filteredVATId[2] +
@@ -82,32 +82,32 @@ public struct VATIdValidator {
             6 * filteredVATId[7] +
             7 * filteredVATId[8]) % 11
     }
-    
+
 }
 
 extension BinaryInteger {
-    
+
     /**
      Is valid VAT identifier.
      */
     public var isValidVATId: Bool { (try? VATIdValidator(self).validate()) != nil }
-    
+
 }
 
 extension StringLiteralType {
-    
+
     /**
     Is valid VAT identifier.
     */
     public var isValidVATId: Bool { (try? VATIdValidator(self).validate()) != nil }
-    
+
 }
 
 extension Double {
-    
+
     /**
     Is valid VAT identifier.
     */
     public var isValidVATId: Bool { (try? VATIdValidator(self).validate()) != nil }
-    
+
 }
